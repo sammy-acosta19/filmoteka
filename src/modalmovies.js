e5654a52f6425915c68a7a61611bc3078e07582e
Maria Fernanda Diaz <mafediazca@gmail.com>
2023-10-30 05:40:40 -0500

    primer itento modal

---
+++ src/modalmovies.js
@@ -0,0 +1,42 @@
+const apiKey = "d62e671e72a3270f6005a951e144404c"
+function showMovieModal(movieId) {
+  fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("movieTitle").textContent = data.title;
+      document.getElementById("movieOverview").textContent = data.overview;
+    });
+  document.getElementById("movieModal").style.display = "block";
+}
+
+document.getElementById("closeModal").addEventListener("click", () => {
+  document.getElementById("movieModal").style.display = "none";
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    document.getElementById("movieModal").style.display = "none";
+  }
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === document.getElementById("movieModal")) {
+    document.getElementById("movieModal").style.display = "none";
+  }
+});
+
+document.getElementById("addToWatched").addEventListener("click", () => {
+  const movieTitle = document.getElementById("movieTitle").textContent;
+  let watchedMovies = JSON.parse(localStorage.getItem("watchedMovies")) || [];
+  watchedMovies.push(movieTitle);
+  localStorage.setItem("watchedMovies", JSON.stringify(watchedMovies));
+  alert("Película añadida a Vistas.");
+});
+
+document.getElementById("addToQueue").addEventListener("click", () => {
+  const movieTitle = document.getElementById("movieTitle").textContent;
+  let queueMovies = JSON.parse(localStorage.getItem("queueMovies")) || [];
+  queueMovies.push(movieTitle);
+  localStorage.setItem("queueMovies", JSON.stringify(queueMovies));
+  alert("Película añadida a la Cola.");
+});bu
