d3b1314b9fb1be1abc8329b7b2b190db536e360c
StivenAguirre <105611680+StivenAguirre@users.noreply.github.com>
2023-11-02 23:31:55 -0500

    commit cambios 4

--- src/index.js
+++ src/index.js
@@ -1,121 +1,114 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const API_KEY = "d62e671e72a3270f6005a951e144404c"; // Reemplaza con tu clave de API de TMDb
-    const API_URL = "https://api.themoviedb.org/3";
-    const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
-  
-    const movieListContainer = document.getElementById("movie-list");
+  const API_KEY = "d62e671e72a3270f6005a951e144404c";
+  const API_URL = "https://api.themoviedb.org/3";
+  const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
-    
-    const moviesPerPage = 9; // Cantidad de películas por página
-    let currentPage = 1; // Página actual
-  
-    let movieIdCounter = 0;
-    
-    const shownMovies = new Set();
+  const movieListContainer = document.getElementById("movie-list");
+  movieListContainer.style.display = "flex";
+  movieListContainer.style.flexWrap = "wrap";
+  movieListContainer.style.justifyContent = "center";
 
-    async function fetchPopularMovies() {
-      try {
-        const response = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`);
-        const data = await response.json();
-  
-        // Mostrar las películas populares
-        data.results.slice(0, moviesPerPage).forEach(async (movie) => {
+  const moviesPerPage = 9;
+  let currentPage = 1;
 
-          const movieContainer = document.createElement("div");
-          movieContainer.id = "btn-open-modal"
-          movieContainer.style.width = "274px"
-          movieContainer.style.height = "480px"
-          movieContainer.style.marginBottom = "-70px"
-          movieContainer.style.marginTop = "100px"
-          movieContainer.style.marginLeft = "40px"
+  const moviesShown = new Set(); // Usaremos un Set para evitar repeticiones
 
+  async function fetchPopularMovies() {
+    try {
+      const response = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`);
+      const data = await response.json();
 
-          const image = document.createElement("img");
-          image.src = IMAGE_BASE_URL + movie.poster_path;
-          image.alt = movie.title;
-          image.style.borderRadius = "5px"
+      data.results.slice(0, moviesPerPage).forEach(async (movie) => {
+        const movieId = movie.id;
 
-  
-          const title = document.createElement("h2");
-          title.textContent = movie.title;
-          title.style.color = "black";
-          title.style.fontFamily = "Roboto";
-          title.style.fontWeight = "500px";
-          title.style.fontSize = "15px"
+        if (!moviesShown.has(movieId)) {
+           const movieContainer = document.createElement("div");
+        movieContainer.id = `movie-${movie.id}`;
+        movieContainer.classList.add("movie");
+        movieContainer.style.width = "274px";
+        movieContainer.style.height = "480px";
+        movieContainer.style.marginBottom = "-70px";
+        movieContainer.style.marginTop = "100px";
+        movieContainer.style.marginLeft = "40px";
 
-          const divcontainerPreInfo = document.createElement("div")
-          divcontainerPreInfo.style.display = "flex"
-          divcontainerPreInfo.style.justifyContent = "space-betwen"
+        const image = document.createElement("img");
+        image.src = IMAGE_BASE_URL + movie.poster_path;
+        image.alt = movie.title;
+        image.style.borderRadius = "5px";
 
-          const genre = document.createElement("p");
-          const genresResponse = await fetch(`${API_URL}/movie/${movie.id}?api_key=${API_KEY}`);
-          const genresData = await genresResponse.json();
-          const genreNames = genresData.genres.map((genre) => genre.name).join(", ");
-          genre.textContent = `${genreNames}     |`;
-          genre.style.color = "rgba(255, 107, 1, 1)";
-          genre.style.fontFamily = "Roboto";
-          genre.style.fontWeight = "500px";
-          genre.style.fontSize = "15px"
+        const title = document.createElement("h2");
+        title.textContent = movie.title;
+        title.style.color = "black";
+        title.style.fontFamily = "Roboto";
+        title.style.fontWeight = "500px";
+        title.style.fontSize = "15px";
 
-          const releaseYear = document.createElement("p");
-          const releaseDate = new Date(movie.release_date);
-          releaseYear.textContent = `${releaseDate.getFullYear()}`;
-          releaseYear.style.color = "rgba(255, 107, 1, 1)";
-          releaseYear.style.fontFamily = "Roboto";
-          releaseYear.style.fontWeight = "500px";
-          releaseYear.style.fontSize = "15px"
-          releaseYear.style.marginLeft = "5px"
-          releaseYear.style.marginRight = "5px"
+        const divcontainerPreInfo = document.createElement("div");
+        divcontainerPreInfo.style.display = "flex";
 
-          const containerRatingOrange = document.createElement("div");
-          containerRatingOrange.style.backgroundColor = "rgba(255, 107, 1, 1)";
-          containerRatingOrange.style.width = "36px";
-          containerRatingOrange.style.height = "16px";
-          containerRatingOrange.style.borderRadius = "5px";
-          
-          const rating = document.createElement("p");
-          rating.textContent = `${movie.vote_average}`;
-          rating.style.color = "white";
-          rating.style.fontFamily = "Roboto";
-          rating.style.fontWeight = "500px";
-          rating.style.fontSize = "13px"
-          rating.style.textAlign = "center"
+        const genre = document.createElement("p");
+        const genresResponse = await fetch(`${API_URL}/movie/${movie.id}?api_key=${API_KEY}`);
+        const genresData = await genresResponse.json();
+        const genreNames = genresData.genres.map((genre) => genre.name).join(", ");
+        genre.textContent = `${genreNames}     |`;
+        genre.style.color = "rgba(255, 107, 1, 1)";
+        genre.style.fontFamily = "Roboto";
+        genre.style.fontWeight = "500px";
+        genre.style.fontSize = "15px";
 
-          movieListContainer.appendChild(movieContainer)
-          movieContainer.appendChild(image);
-          movieContainer.appendChild(title);
-          movieContainer.appendChild(divcontainerPreInfo)
-          divcontainerPreInfo.appendChild(genre);
-          divcontainerPreInfo.appendChild(releaseYear);
-          divcontainerPreInfo.appendChild(containerRatingOrange)
-          containerRatingOrange.appendChild(rating)
-        
+        const releaseYear = document.createElement("p");
+        const releaseDate = new Date(movie.release_date);
+        releaseYear.textContent = `${releaseDate.getFullYear()}`;
+        releaseYear.style.color = "rgba(255, 107, 1, 1)";
+        releaseYear.style.fontFamily = "Roboto";
+        releaseYear.style.fontWeight = "500px";
+        releaseYear.style.fontSize = "15px";
+        releaseYear.style.marginLeft = "5px";
+        releaseYear.style.marginRight = "5px";
 
-          const btnAbrirModal = movieContainer
-          const btnCerrarModal = document.getElementById("btn-close-modal");
-          const modal = document.querySelector('#modal');
+        const containerRatingOrange = document.createElement("div");
+        containerRatingOrange.style.backgroundColor = "rgba(255, 107, 1, 1)";
+        containerRatingOrange.style.width = "36px";
+        containerRatingOrange.style.height = "16px";
+        containerRatingOrange.style.borderRadius = "5px";
 
-          btnAbrirModal.addEventListener('click', () => {
-            modal.showModal();
-          });
+        const rating = document.createElement("p");
+        rating.textContent = `${movie.vote_average}`;
+        rating.style.color = "white";
+        rating.style.fontFamily = "Roboto";
+        rating.style.fontWeight = "500px";
+        rating.style.fontSize = "13px";
+        rating.style.textAlign = "center";
 
-          btnCerrarModal.addEventListener('click', () => {
-            modal.close();
-          }); 
+        movieListContainer.appendChild(movieContainer);
+        movieContainer.appendChild(image);
+        movieContainer.appendChild(title);
+        movieContainer.appendChild(divcontainerPreInfo);
+        divcontainerPreInfo.appendChild(genre);
+        divcontainerPreInfo.appendChild(releaseYear);
+        divcontainerPreInfo.appendChild(containerRatingOrange);
+        containerRatingOrange.appendChild(rating);
 
-    
+        const btnAbrirModal = movieContainer;
+        const btnCerrarModal = document.getElementById("btn-close-modal");
+        const modal = document.querySelector('#modal');
+
+        btnAbrirModal.addEventListener('click', () => {
+          modal.showModal();
         });
-      } catch (error) {
-        console.log("Error al obtener películas populares:", error);
-      }
+
+        btnCerrarModal.addEventListener('click', () => {
+          modal.close();
+        });
+        }
+      });
+    } catch (error) {
+      console.log("Error al obtener películas populares:", error);
     }
-        fetchPopularMovies(currentPage);
+  }
 
-    });
-  
-  
-
-
+  fetchPopularMovies(currentPage);
+});
 
 // Boton my library 
 const btnMyLibrary = document.querySelector('.my-library-btn');
